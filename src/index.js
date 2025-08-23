10dfa5a70102066437401a305334c822d5614aca
InnaKolesnykova <149619711+InnaKolesnykova@users.noreply.github.com>
2025-08-23 19:13:14 +0300

    v
    v

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import ReactDOM from 'react-dom';
-import { App } from 'components/App';
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter from react-router-dom
 import { Provider } from 'react-redux';
-import {store} from './redux/store';
+import { store } from './redux/store';
+import { App } from './components/App';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <BrowserRouter basename="/goit-react-hw-08-phonebook">
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
