4ee0a10d1856f72ef8c2832da8a8103b8c2c65db
InnaKolesnykova <149619711+InnaKolesnykova@users.noreply.github.com>
2025-08-28 19:26:19 +0300

    м
    м

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,48 +1,56 @@
-import React from 'react';
-import { lazy } from 'react';
+import React, { useEffect, lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { Suspense } from 'react';
+import { useDispatch } from 'react-redux';
 
 import { Layout } from './Layout/Layout';
 import PrivateRoute from './PrivateRoute/PrivateRoute';
 import RestrictedRoute from './RestrictedRoute/RestrictedRoute';
 import { Loader } from './Loader/Loader';
+import { getCurrentUser } from '../redux/authSlice'; // ⚡ твоя санка
 
 const HomePage = lazy(() => import('../components/Home/Home'));
 const RegisterPage = lazy(() => import('../components/RegisterPage/RegisterPage'));
 const LoginPage = lazy(() => import('../components/LoginPage/LoginPage'));
 const ContactsPage = lazy(() => import('../components/ContactPage/ContactPage'));
 
-
 export const App = () => {
-  
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCurrentUser()); // ⚡ тільки тут
+  }, [dispatch]);
 
   return (
-    <>
-        <Suspense fallback={<Loader />}>
-          <Routes>
-            <Route path='/' element={<Layout /> }>
-              <Route 
-                index 
-                element={<HomePage />} />
-              <Route 
-                path="/login" 
-                element={<RestrictedRoute>
-                          <LoginPage />
-                        </RestrictedRoute>} />
-              <Route 
-                path="/register" 
-                element={<RestrictedRoute>
-                          <RegisterPage />
-                        </RestrictedRoute>} />
-              <Route 
-                path="/contacts" 
-                element={<PrivateRoute>
-                  <ContactsPage />
-                  </PrivateRoute>} />
-            </Route> 
-          </Routes>
-        </Suspense>
-      </>
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route
+            path="/login"
+            element={
+              <RestrictedRoute>
+                <LoginPage />
+              </RestrictedRoute>
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <RestrictedRoute>
+                <RegisterPage />
+              </RestrictedRoute>
+            }
+          />
+          <Route
+            path="/contacts"
+            element={
+              <PrivateRoute>
+                <ContactsPage />
+              </PrivateRoute>
+            }
+          />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
